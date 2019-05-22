7d6bb8dfe2406275ca2a406822c6ff5a5fa8f883
Jookeret <s5935512034@phuket.psu.ac.th>
2019-05-22 11:10:10 +0700

    new var

--- server/serch.js
+++ server/serch.js
@@ -85,6 +85,7 @@ const countData = async (dateUrl) => {
         //console.log(" resultid", resultid)
         resultid = match.atac(lines) //เลือกข้อมูลที่มีการเชื่อมต่อ (522008) แล้วเก็บไว้เป็น array
         // console.log(" resulti2", resultid)
+        let unusedVar = 'sdasdsa'
 
         let n = (resultid === undefined) ? 0 : resultid.length
 
